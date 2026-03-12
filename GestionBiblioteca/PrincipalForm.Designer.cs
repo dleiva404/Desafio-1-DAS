997a955e235ce4e25121b07aaf09f40f2b55e31c
dleiva404 <davidleiva79@gmail.com>
2026-03-12 08:31:47 -0600

    Interfaz y funcionalidad

--- GestionBiblioteca/PrincipalForm.Designer.cs
+++ GestionBiblioteca/PrincipalForm.Designer.cs
@@ -30,26 +30,26 @@
         {
             TabControl1 = new TabControl();
             TabLibros = new TabPage();
+            txtAutor = new TextBox();
+            txtAnio = new TextBox();
+            txtGenero = new TextBox();
+            txtTitulo = new TextBox();
+            lblGenero = new Label();
+            lblAnio = new Label();
+            lblAutor = new Label();
+            lblTitulo = new Label();
+            panelBotonesLibros = new Panel();
+            btnLimpiarLibro = new Button();
+            btnEliminarLibro = new Button();
+            btnEditarLibro = new Button();
+            btnAgregarLibro = new Button();
+            dgvLibros = new DataGridView();
             TabUsuarios = new TabPage();
             TabPrestamos = new TabPage();
-            dgvLibros = new DataGridView();
-            panelBotonesLibros = new Panel();
-            btnAgregarLibro = new Button();
-            btnEditarLibro = new Button();
-            btnEliminarLibro = new Button();
-            btnLimpiarLibro = new Button();
-            lblTitulo = new Label();
-            lblAutor = new Label();
-            lblAnio = new Label();
-            lblGenero = new Label();
-            textTitulo = new TextBox();
-            txtGenero = new TextBox();
-            txtAnio = new TextBox();
-            txtAutor = new TextBox();
             TabControl1.SuspendLayout();
             TabLibros.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)dgvLibros).BeginInit();
             panelBotonesLibros.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvLibros).BeginInit();
             SuspendLayout();
             // 
             // TabControl1
@@ -69,7 +69,7 @@
             TabLibros.Controls.Add(txtAutor);
             TabLibros.Controls.Add(txtAnio);
             TabLibros.Controls.Add(txtGenero);
-            TabLibros.Controls.Add(textTitulo);
+            TabLibros.Controls.Add(txtTitulo);
             TabLibros.Controls.Add(lblGenero);
             TabLibros.Controls.Add(lblAnio);
             TabLibros.Controls.Add(lblAutor);
@@ -83,6 +83,132 @@
             TabLibros.TabIndex = 0;
             TabLibros.Text = "Libros";
             TabLibros.UseVisualStyleBackColor = true;
+            // 
+            // txtAutor
+            // 
+            txtAutor.Location = new Point(338, 357);
+            txtAutor.Name = "txtAutor";
+            txtAutor.Size = new Size(100, 23);
+            txtAutor.TabIndex = 4;
+            // 
+            // txtAnio
+            // 
+            txtAnio.Location = new Point(541, 357);
+            txtAnio.Name = "txtAnio";
+            txtAnio.Size = new Size(100, 23);
+            txtAnio.TabIndex = 5;
+            // 
+            // txtGenero
+            // 
+            txtGenero.Location = new Point(752, 357);
+            txtGenero.Name = "txtGenero";
+            txtGenero.Size = new Size(100, 23);
+            txtGenero.TabIndex = 6;
+            // 
+            // txtTitulo
+            // 
+            txtTitulo.Location = new Point(117, 357);
+            txtTitulo.Name = "txtTitulo";
+            txtTitulo.Size = new Size(100, 23);
+            txtTitulo.TabIndex = 3;
+            // 
+            // lblGenero
+            // 
+            lblGenero.AutoSize = true;
+            lblGenero.Location = new Point(692, 365);
+            lblGenero.Name = "lblGenero";
+            lblGenero.Size = new Size(54, 15);
+            lblGenero.TabIndex = 5;
+            lblGenero.Text = "Género : ";
+            // 
+            // lblAnio
+            // 
+            lblAnio.AutoSize = true;
+            lblAnio.Location = new Point(500, 365);
+            lblAnio.Name = "lblAnio";
+            lblAnio.Size = new Size(35, 15);
+            lblAnio.TabIndex = 4;
+            lblAnio.Text = "Año :";
+            // 
+            // lblAutor
+            // 
+            lblAutor.AutoSize = true;
+            lblAutor.Location = new Point(289, 365);
+            lblAutor.Name = "lblAutor";
+            lblAutor.Size = new Size(43, 15);
+            lblAutor.TabIndex = 3;
+            lblAutor.Text = "Autor :";
+            // 
+            // lblTitulo
+            // 
+            lblTitulo.AutoSize = true;
+            lblTitulo.Location = new Point(67, 365);
+            lblTitulo.Name = "lblTitulo";
+            lblTitulo.Size = new Size(44, 15);
+            lblTitulo.TabIndex = 2;
+            lblTitulo.Text = "Título :";
+            // 
+            // panelBotonesLibros
+            // 
+            panelBotonesLibros.Controls.Add(btnLimpiarLibro);
+            panelBotonesLibros.Controls.Add(btnEliminarLibro);
+            panelBotonesLibros.Controls.Add(btnEditarLibro);
+            panelBotonesLibros.Controls.Add(btnAgregarLibro);
+            panelBotonesLibros.Dock = DockStyle.Bottom;
+            panelBotonesLibros.Location = new Point(3, 480);
+            panelBotonesLibros.Name = "panelBotonesLibros";
+            panelBotonesLibros.Size = new Size(1170, 150);
+            panelBotonesLibros.TabIndex = 6;
+            // 
+            // btnLimpiarLibro
+            // 
+            btnLimpiarLibro.Location = new Point(436, 64);
+            btnLimpiarLibro.Name = "btnLimpiarLibro";
+            btnLimpiarLibro.Size = new Size(75, 23);
+            btnLimpiarLibro.TabIndex = 3;
+            btnLimpiarLibro.Text = "Limpiar";
+            btnLimpiarLibro.UseVisualStyleBackColor = true;
+            btnLimpiarLibro.Click += btnLimpiarLibro_Click;
+            // 
+            // btnEliminarLibro
+            // 
+            btnEliminarLibro.Location = new Point(321, 64);
+            btnEliminarLibro.Name = "btnEliminarLibro";
+            btnEliminarLibro.Size = new Size(75, 23);
+            btnEliminarLibro.TabIndex = 3;
+            btnEliminarLibro.Text = "Eliminar";
+            btnEliminarLibro.UseVisualStyleBackColor = true;
+            btnEliminarLibro.Click += btnEliminarLibro_Click;
+            // 
+            // btnEditarLibro
+            // 
+            btnEditarLibro.Location = new Point(210, 64);
+            btnEditarLibro.Name = "btnEditarLibro";
+            btnEditarLibro.Size = new Size(75, 23);
+            btnEditarLibro.TabIndex = 2;
+            btnEditarLibro.Text = "Editar";
+            btnEditarLibro.UseVisualStyleBackColor = true;
+            btnEditarLibro.Click += btnEditarLibro_Click;
+            // 
+            // btnAgregarLibro
+            // 
+            btnAgregarLibro.Location = new Point(95, 64);
+            btnAgregarLibro.Name = "btnAgregarLibro";
+            btnAgregarLibro.Size = new Size(75, 23);
+            btnAgregarLibro.TabIndex = 1;
+            btnAgregarLibro.Text = "Agregar";
+            btnAgregarLibro.UseVisualStyleBackColor = true;
+            btnAgregarLibro.Click += btnAgregarLibro_Click;
+            // 
+            // dgvLibros
+            // 
+            dgvLibros.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvLibros.Dock = DockStyle.Top;
+            dgvLibros.Location = new Point(3, 3);
+            dgvLibros.Name = "dgvLibros";
+            dgvLibros.Size = new Size(1170, 300);
+            dgvLibros.TabIndex = 1;
+            dgvLibros.CellClick += dgvLibros_CellClick;
             // 
             // TabUsuarios
             // 
@@ -104,128 +230,6 @@
             TabPrestamos.Text = "Préstamos";
             TabPrestamos.UseVisualStyleBackColor = true;
             // 
-            // dgvLibros
-            // 
-            dgvLibros.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvLibros.Dock = DockStyle.Top;
-            dgvLibros.Location = new Point(3, 3);
-            dgvLibros.Name = "dgvLibros";
-            dgvLibros.Size = new Size(1170, 300);
-            dgvLibros.TabIndex = 0;
-            // 
-            // panelBotonesLibros
-            // 
-            panelBotonesLibros.Controls.Add(btnLimpiarLibro);
-            panelBotonesLibros.Controls.Add(btnEliminarLibro);
-            panelBotonesLibros.Controls.Add(btnEditarLibro);
-            panelBotonesLibros.Controls.Add(btnAgregarLibro);
-            panelBotonesLibros.Dock = DockStyle.Bottom;
-            panelBotonesLibros.Location = new Point(3, 480);
-            panelBotonesLibros.Name = "panelBotonesLibros";
-            panelBotonesLibros.Size = new Size(1170, 150);
-            panelBotonesLibros.TabIndex = 1;
-            // 
-            // btnAgregarLibro
-            // 
-            btnAgregarLibro.Location = new Point(95, 64);
-            btnAgregarLibro.Name = "btnAgregarLibro";
-            btnAgregarLibro.Size = new Size(75, 23);
-            btnAgregarLibro.TabIndex = 0;
-            btnAgregarLibro.Text = "Agregar";
-            btnAgregarLibro.UseVisualStyleBackColor = true;
-            btnAgregarLibro.Click += button1_Click;
-            // 
-            // btnEditarLibro
-            // 
-            btnEditarLibro.Location = new Point(210, 64);
-            btnEditarLibro.Name = "btnEditarLibro";
-            btnEditarLibro.Size = new Size(75, 23);
-            btnEditarLibro.TabIndex = 1;
-            btnEditarLibro.Text = "Editar";
-            btnEditarLibro.UseVisualStyleBackColor = true;
-            // 
-            // btnEliminarLibro
-            // 
-            btnEliminarLibro.Location = new Point(321, 64);
-            btnEliminarLibro.Name = "btnEliminarLibro";
-            btnEliminarLibro.Size = new Size(75, 23);
-            btnEliminarLibro.TabIndex = 2;
-            btnEliminarLibro.Text = "Eliminar";
-            btnEliminarLibro.UseVisualStyleBackColor = true;
-            // 
-            // btnLimpiarLibro
-            // 
-            btnLimpiarLibro.Location = new Point(436, 64);
-            btnLimpiarLibro.Name = "btnLimpiarLibro";
-            btnLimpiarLibro.Size = new Size(75, 23);
-            btnLimpiarLibro.TabIndex = 3;
-            btnLimpiarLibro.Text = "Limpiar";
-            btnLimpiarLibro.UseVisualStyleBackColor = true;
-            // 
-            // lblTitulo
-            // 
-            lblTitulo.AutoSize = true;
-            lblTitulo.Location = new Point(67, 365);
-            lblTitulo.Name = "lblTitulo";
-            lblTitulo.Size = new Size(44, 15);
-            lblTitulo.TabIndex = 2;
-            lblTitulo.Text = "Título :";
-            // 
-            // lblAutor
-            // 
-            lblAutor.AutoSize = true;
-            lblAutor.Location = new Point(289, 365);
-            lblAutor.Name = "lblAutor";
-            lblAutor.Size = new Size(43, 15);
-            lblAutor.TabIndex = 3;
-            lblAutor.Text = "Autor :";
-            // 
-            // lblAnio
-            // 
-            lblAnio.AutoSize = true;
-            lblAnio.Location = new Point(500, 365);
-            lblAnio.Name = "lblAnio";
-            lblAnio.Size = new Size(35, 15);
-            lblAnio.TabIndex = 4;
-            lblAnio.Text = "Año :";
-            // 
-            // lblGenero
-            // 
-            lblGenero.AutoSize = true;
-            lblGenero.Location = new Point(692, 365);
-            lblGenero.Name = "lblGenero";
-            lblGenero.Size = new Size(54, 15);
-            lblGenero.TabIndex = 5;
-            lblGenero.Text = "Género : ";
-            // 
-            // textTitulo
-            // 
-            textTitulo.Location = new Point(117, 357);
-            textTitulo.Name = "textTitulo";
-            textTitulo.Size = new Size(100, 23);
-            textTitulo.TabIndex = 6;
-            // 
-            // txtGenero
-            // 
-            txtGenero.Location = new Point(752, 357);
-            txtGenero.Name = "txtGenero";
-            txtGenero.Size = new Size(100, 23);
-            txtGenero.TabIndex = 7;
-            // 
-            // txtAnio
-            // 
-            txtAnio.Location = new Point(541, 357);
-            txtAnio.Name = "txtAnio";
-            txtAnio.Size = new Size(100, 23);
-            txtAnio.TabIndex = 8;
-            // 
-            // txtAutor
-            // 
-            txtAutor.Location = new Point(338, 357);
-            txtAutor.Name = "txtAutor";
-            txtAutor.Size = new Size(100, 23);
-            txtAutor.TabIndex = 9;
-            // 
             // PrincipalForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -239,8 +243,8 @@
             TabControl1.ResumeLayout(false);
             TabLibros.ResumeLayout(false);
             TabLibros.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)dgvLibros).EndInit();
             panelBotonesLibros.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dgvLibros).EndInit();
             ResumeLayout(false);
         }
 
@@ -263,6 +267,6 @@
         private TextBox txtAutor;
         private TextBox txtAnio;
         private TextBox txtGenero;
-        private TextBox textTitulo;
+        private TextBox txtTitulo;
     }
 }
--- GestionBiblioteca/PrincipalForm.cs
+++ GestionBiblioteca/PrincipalForm.cs
@@ -1,15 +1,112 @@
+using GestionBiblioteca.Models;
+using GestionBiblioteca.Services;
+
 namespace GestionBiblioteca
 {
     public partial class PrincipalForm : Form
     {
+        private LibroService _libroService = new LibroService();
+        private UsuarioService _usuarioService = new UsuarioService();
+        private PrestamoService _prestamoService = new PrestamoService();
+
         public PrincipalForm()
         {
             InitializeComponent();
+            CargarLibros();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        // ===== LIBROS =====
+        private void CargarLibros()
         {
+            dgvLibros.DataSource = null;
+            dgvLibros.DataSource = _libroService.ObtenerTodos();
+        }
 
+        private void btnAgregarLibro_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtTitulo.Text) ||
+                string.IsNullOrWhiteSpace(txtAutor.Text) ||
+                string.IsNullOrWhiteSpace(txtAnio.Text) ||
+                string.IsNullOrWhiteSpace(txtGenero.Text))
+            {
+                MessageBox.Show("Todos los campos son obligatorios.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!int.TryParse(txtAnio.Text, out int anio))
+            {
+                MessageBox.Show("El año debe ser un número.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            _libroService.Agregar(txtTitulo.Text, txtAutor.Text, anio, txtGenero.Text);
+            CargarLibros();
+            LimpiarCamposLibro();
+            MessageBox.Show("Libro agregado correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnEditarLibro_Click(object sender, EventArgs e)
+        {
+            if (dgvLibros.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Selecciona un libro para editar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int id = (int)dgvLibros.SelectedRows[0].Cells["Id"].Value;
+            if (!int.TryParse(txtAnio.Text, out int anio))
+            {
+                MessageBox.Show("El año debe ser un número.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            _libroService.Actualizar(id, txtTitulo.Text, txtAutor.Text, anio, txtGenero.Text);
+            CargarLibros();
+            LimpiarCamposLibro();
+            MessageBox.Show("Libro actualizado correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnEliminarLibro_Click(object sender, EventArgs e)
+        {
+            if (dgvLibros.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Selecciona un libro para eliminar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int id = (int)dgvLibros.SelectedRows[0].Cells["Id"].Value;
+            var confirmacion = MessageBox.Show("¿Estás seguro de eliminar este libro?", "Confirmar", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (confirmacion == DialogResult.Yes)
+            {
+                _libroService.Eliminar(id);
+                CargarLibros();
+                LimpiarCamposLibro();
+            }
+        }
+
+        private void btnLimpiarLibro_Click(object sender, EventArgs e)
+        {
+            LimpiarCamposLibro();
+        }
+
+        private void LimpiarCamposLibro()
+        {
+            txtTitulo.Text = "";
+            txtAutor.Text = "";
+            txtAnio.Text = "";
+            txtGenero.Text = "";
+        }
+
+        private void dgvLibros_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                var row = dgvLibros.Rows[e.RowIndex];
+                txtTitulo.Text = row.Cells["Titulo"].Value?.ToString();
+                txtAutor.Text = row.Cells["Autor"].Value?.ToString();
+                txtAnio.Text = row.Cells["Anio"].Value?.ToString();
+                txtGenero.Text = row.Cells["Genero"].Value?.ToString();
+            }
         }
     }
 }
